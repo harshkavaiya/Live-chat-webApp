84b12e115d087fe50e4ce6878500f6d13d948d45
Hardik Vatukiya <hardikvatukiya0014@gmail.com>
2024-12-21 16:09:46 +0530

    design chat page and add some funcationality

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -7,18 +7,18 @@ import SideSetting from "../components/SideSetting";
 const Home = () => {
   const [userselected, setuserselect] = useState(false);
   return (
-    <div className="h-screen w-screen flex">
+    <div className="h-screen w-screen flex gap-0 transition-all duration-200">
       {/* user setting */}
-      <div className="w-[5%]">
+      <div className="w-[5rem] hidden sm:block">
         <SideSetting />
       </div>
       {/* Contact List */}
-      <div className="sm:w-[40%] w-full overflow-hidden">
+      <div className="w-full sm:w-[50%] overflow-hidden">
         <Sidebar />
       </div>
 
       {/* Message Area */}
-      <div className="sm:block flex-1 hidden bg-accent-content">
+      <div className="hidden sm:block w-[60%]  bg-accent-content">
         {userselected ? <ChatPage /> : <NochatSelect />}
       </div>
     </div>
