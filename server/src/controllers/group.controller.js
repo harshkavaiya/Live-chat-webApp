b2eb6683df6cc2ca2be177ae48f182f33ee22ecb
Harsh412589 <hkavaiya001@gmail.com>
2025-02-09 20:45:06 +0530

    add some group in front

--- server/src/controllers/group.controller.js
+++ server/src/controllers/group.controller.js
@@ -1,30 +1,40 @@
 import cloudinary from "../lib/cloudinary.js";
 import Group from "../models/group.model.js";
 export const createGroup = async (req, res) => {
+  console.log("Received File:", req.file);
+  console.log("Request Body:", req.body);
+
   try {
     const { name, description, type } = req.body;
     const admin = req.user._id;
+
     if (!name) {
       return res
-        .status(250)
-        .json({ success: false, message: "Name field required" });
+        .status(400)
+        .json({ success: false, message: "Group Name is required" });
     }
-    let photoUrl = "";
+
+    const membersArray = JSON.parse(req.body.members || "[]");
+
+    let photoUrl = null;
     if (req.file) {
-      const result = await cloudinary.uploader.upload(req.file.path);
+      const result = await cloudinary.uploader.upload(req.file.path, {
+        folder: "group_photos",
+      });
       photoUrl = result.secure_url;
     }
 
-    const inviteLink = `${Math.random().toString(36).substr(2, 8)}`;
+    const inviteLink = Math.random().toString(36).substr(2, 8);
     const newGroup = new Group({
       name,
       description,
       photo: photoUrl,
       admin,
-      members: [admin],
+      members: [admin, ...membersArray],
       type,
       inviteLink,
     });
+
     await newGroup.save();
     res.status(200).json({ success: true, newGroup });
   } catch (error) {
@@ -33,7 +43,7 @@ export const createGroup = async (req, res) => {
         .status(400)
         .json({ message: "You already have a group with this name." });
     }
-    console.log("error in create group controller: ", error.message);
+    console.log("Error in createGroup:", error.message);
     res.status(500).json({ success: false, message: "Server error" });
   }
 };
--- server/src/middleware/uploadGP.middleware.js
+++ server/src/middleware/uploadGP.middleware.js
@@ -10,6 +10,15 @@ const storage = new CloudinaryStorage({
   },
 });
 
-const upload = multer({ storage });
+const upload = multer({
+  storage,
+  limits: { fileSize: 5 * 1024 * 1024 }, // 5MB limit
+  fileFilter: (req, file, cb) => {
+    if (!file.mimetype.startsWith("image/")) {
+      return cb(new Error("Only images are allowed"), false);
+    }
+    cb(null, true);
+  },
+});
 
 export default upload;
