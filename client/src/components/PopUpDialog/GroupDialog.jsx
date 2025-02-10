7ff8daff3919595cd872accf9adcfb796a633e76
Harsh412589 <hkavaiya001@gmail.com>
2025-02-10 19:09:58 +0530

    group photo upload

--- client/src/components/PopUpDialog/GroupDialog.jsx
+++ client/src/components/PopUpDialog/GroupDialog.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { FaArrowLeft } from "react-icons/fa6";
 import { FiUsers, FiUpload } from "react-icons/fi";
 import { MdOutlineCloudUpload } from "react-icons/md";
@@ -11,53 +11,66 @@ const GroupDialog = () => {
     description: "",
     selectedUsers: [],
   });
-
-  const [image, setImage] = useState(null);
-  const [file, setFile] = useState(null);
+  const fileInputRef = useRef(null);
+  const [photo, setPhoto] = useState(null);
+  const [groupType, setGroupType] = useState("");
   const [isOpen, setIsOpenDialog] = useState(false);
 
-  const closeDialog = () => document.getElementById("my_modal_5").close();
+  const closeDialog = () => {
+    setFormData({ groupName: "", description: "", selectedUsers: [] });
+    setPhoto(null);
+    setGroupType("");
+    document.getElementById("my_modal_5").close();
+  };
+
   const userDialog = () => {
     setIsOpenDialog(true);
     document.getElementById("addUsers").showModal();
   };
 
   const handleFileChange = (event) => {
-    const selectedFile = event.target.files[0];
-    if (selectedFile) {
+    const file = event.target.files[0];
+
+    if (file) {
       const reader = new FileReader();
-      reader.onloadend = () => setImage(reader.result);
-      reader.readAsDataURL(selectedFile);
-      setFile(selectedFile);
+
+      reader.onloadend = () => {
+        setPhoto(reader.result);
+      };
+
+      reader.readAsDataURL(file);
     }
   };
 
   const handleInputChange = (e) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
   };
 
   const handleCreateGroup = async () => {
-    if (!formData.groupName.trim()) {
-      alert("Group Name is required!");
+    if (!formData.groupName) {
+      alert("Group name is required!");
       return;
     }
 
-    const data = new FormData();
-    data.append("name", formData.groupName);
-    data.append("description", formData.description);
-    data.append(
-      "members",
-      JSON.stringify(formData.selectedUsers.map((u) => u.id))
-    );
-    if (file) data.append("photo", file);
+    const data = {
+      name: formData.groupName,
+      description: formData.description,
+      type: groupType,
+      photo,
+      members: JSON.stringify(formData.selectedUsers.map((user) => user.id)),
+    };
 
     try {
       const response = await axiosInstance.post("/group/create", data, {
-        headers: { "Content-Type": "multipart/form-data" },
+        headers: { "Content-Type": "application/json" },
       });
+      if (!response.data.success) {
+        alert(response.data.message);
+        return;
+      }
 
       console.log("Group created:", response.data);
-      alert("Group successfully created!");
       closeDialog();
     } catch (error) {
       console.error(
@@ -96,62 +109,78 @@ const GroupDialog = () => {
 
         {/* Group Details */}
         <div className="flex flex-col space-y-4">
-          <div className="flex space-x-4 items-center">
+          <div className="flex justify-center items-center">
             <div
-              className={`w-20 h-20 rounded-full flex items-center justify-center overflow-hidden ${
-                !image && "border"
-              }`}
+              onClick={() => fileInputRef.current.click()}
+              className={`w-20 h-20 rounded-full flex items-center cursor-pointer justify-center overflow-hidden border`}
             >
-              {image ? (
+              {photo ? (
                 <img
-                  src={image}
+                  src={photo}
                   alt="Group Avatar"
                   className="object-cover w-full h-full"
                 />
               ) : (
                 <MdOutlineCloudUpload size={22} />
               )}
-            </div>
-            <label htmlFor="file-input" className="btn">
-              <FiUpload size={15} />
               <input
                 type="file"
-                id="file-input"
+                ref={fileInputRef}
                 accept="image/*"
                 onChange={handleFileChange}
                 className="hidden"
               />
-            </label>
+            </div>
           </div>
 
           <input
             type="text"
             name="groupName"
             placeholder="Enter group name"
-            className="input input-bordered"
+            className="input input-bordered focus:outline-none focus:ring-1 focus:ring-primary focus:border-primary transition"
             value={formData.groupName}
             onChange={handleInputChange}
           />
           <textarea
             name="description"
             placeholder="What's this group about?"
-            className="w-full textarea textarea-bordered"
+            className="w-full textarea textarea-bordered focus:outline-none focus:ring-1 focus:ring-primary focus:border-primary transition"
             rows={2}
             value={formData.description}
             onChange={handleInputChange}
           />
-
-          <div className="grid grid-cols-3 gap-2">
-            {formData.selectedUsers.map((user) => (
-              <p key={user.id} className="text-center">
-                {user.fullname}
-              </p>
-            ))}
+          <div className="flex gap-4 w-full">
+            <div className="flex-1">
+              <label htmlFor="groupType" className="text-sm">
+                Select group type
+              </label>
+              <select
+                className="select select-bordered w-full border py-2 px-3 shadow-sm focus:outline-none focus:ring-1 focus:ring-primary focus:border-primary transition"
+                id="groupType"
+                value={groupType}
+                onChange={(e) => setGroupType(e.target.value)}
+              >
+                <option value="" disabled>
+                  Choose Type
+                </option>
+                <option value="private">🔒 Private</option>
+                <option value="public">🌍 Public</option>
+              </select>
+            </div>
+            <div className="w-1/4">
+              <label className="text-sm">Selected Users</label>
+              <input
+                type="text"
+                value={formData.selectedUsers.length}
+                className="input input-bordered text-center w-full"
+                disabled
+              />
+            </div>
           </div>
         </div>
 
         {/* Buttons */}
-        <div className="flex items-center gap-5 fixed w-full left-0 p-5 bottom-0">
+        <div className="flex bg-base-100 items-center gap-5 fixed w-full left-0 px-5 pb-5 pt-1 bottom-0">
           <button className="btn btn-error flex-1" onClick={closeDialog}>
             Cancel
           </button>
--- server/src/controllers/group.controller.js
+++ server/src/controllers/group.controller.js
@@ -1,11 +1,8 @@
 import cloudinary from "../lib/cloudinary.js";
 import Group from "../models/group.model.js";
 export const createGroup = async (req, res) => {
-  console.log("Received File:", req.file);
-  console.log("Request Body:", req.body);
-
   try {
-    const { name, description, type } = req.body;
+    const { name, description, type, photo, members } = req.body;
     const admin = req.user._id;
 
     if (!name) {
@@ -14,21 +11,21 @@ export const createGroup = async (req, res) => {
         .json({ success: false, message: "Group Name is required" });
     }
 
-    const membersArray = JSON.parse(req.body.members || "[]");
-
-    let photoUrl = null;
-    if (req.file) {
-      const result = await cloudinary.uploader.upload(req.file.path, {
-        folder: "group_photos",
-      });
-      photoUrl = result.secure_url;
+    const membersArray = JSON.parse(members || "[]");
+    let cloudPic = null;
+    if (photo) {
+      cloudPic = await cloudinary.uploader.upload(photo);
+    } else {
+      cloudPic = {
+        secure_url: "",
+      };
     }
 
     const inviteLink = Math.random().toString(36).substr(2, 8);
     const newGroup = new Group({
       name,
       description,
-      photo: photoUrl,
+      photo: cloudPic.secure_url,
       admin,
       members: [admin, ...membersArray],
       type,
