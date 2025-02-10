c91b2d9ab2aed18793f8fca7d1717d124c2c2ee5
Hardik Vatukiya <hardikvatukiya0014@gmail.com>
2025-02-10 09:09:22 +0530

    remove multer-clounary

--- server/src/routes/group.route.js
+++ server/src/routes/group.route.js
@@ -1,10 +1,9 @@
 import express from "express";
 import { AuthRoute } from "../middleware/auth.middleware.js";
 import { createGroup } from "../controllers/group.controller.js";
-import upload from "../middleware/uploadGP.middleware.js";
 
 const router = express.Router();
 
-router.post("/create", AuthRoute, upload.single("photo"), createGroup);
+router.post("/create", AuthRoute, createGroup);
 
 export default router;
