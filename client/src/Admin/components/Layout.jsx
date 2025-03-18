7307f2b8fa37a4555173b4acf801b4153e1b50b8
Harsh412589 <hkavaiya001@gmail.com>
2025-03-18 19:18:49 +0530

    admin theme

--- client/src/Admin/components/Layout.jsx
+++ client/src/Admin/components/Layout.jsx
@@ -2,11 +2,14 @@ import { Toaster } from "react-hot-toast";
 import Home from "../Home";
 import LoginPage from "../Login";
 import useAuthAdmin from "../store/useAuthAdmin";
+import { useContext } from "react";
+import { ThemeContext } from "../../GlobalStates/ThemeContext";
 
 const Layout = () => {
   const { user } = useAuthAdmin();
+  const { theme } = useContext(ThemeContext);
   return (
-    <div>
+    <div data-theme={theme}>
       <Toaster />
       {user ? <Home /> : <LoginPage />}
     </div>
